612c548474be8dcf3e98db32b7c7e13ef645e888
sunnymiesta <39572876+sunnymiesta@users.noreply.github.com>
2018-06-08 10:46:23 +1000

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,5 +1,5 @@
 
-set(CN_PROJECT_NAME "catawallet")
-set(CN_CURRENCY_DISPLAY_NAME "Catalyst")
-set(CN_CURRENCY_TICKER "CALT")
+set(CN_PROJECT_NAME adinawallet")
+set(CN_CURRENCY_DISPLAY_NAME "Adina")
+set(CN_CURRENCY_TICKER "ADN")
 set(CN_VERSION 0.0.1)
